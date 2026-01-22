f0ada76a811f267a199f5b837a2faa6d38aaebe0
Snehapandey07 <snehapandey.ss99@gmail.com>
2026-01-22 08:31:24 +0530

    updates

--- Arrays/LinearSearch.cpp
+++ Arrays/LinearSearch.cpp
@@ -1,4 +1,4 @@
-// /*Date : 29.01.26*/
+// /*Date : 22.01.26*/
 // /*Linear Search algorithm*/
 
 // #include <iostream>
@@ -16,4 +16,41 @@
     
 // }
 // return 0;
-// }
+//
+
+
+
+
+
+
+/*Date : 23.01.26 */
+/*Linear Search algorithm , gives index value*/
+
+#include <iostream>
+using namespace std;
+
+int LinearSearch (int arr [], int size, int target){
+    for(int i = 0; i< size; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    // int arr[] = {4,2,7,1,8,2,6,};
+    // int size = 7;
+    // int target = 8;
+    int size; 
+    cout<<"enter the size of array: "<<endl;
+    cin>>size;
+    int arr[size] = {};
+    cout<<"enter array : "<<endl;
+    cin>>arr;
+    int target;
+    cout<<"Enter target you want to search : "<<endl;
+    cin>>target
+    cout<< LinearSearch(arr, size, target)<<endl;
+    return 0 ;
+}
+
