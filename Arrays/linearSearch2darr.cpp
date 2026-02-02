f66a187c2010fb4f67ebaec3085de18b225705ec
Snehapandey07 <snehapandey.ss99@gmail.com>
2026-02-02 23:14:03 +0530

    update

---
+++ Arrays/linearSearch2darr.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void LinearSearch(int mat[4][3], int rows, int colms, int key) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < colms; j++) {
+            if (mat[i][j] == key) {
+                cout << "Element " << key << " found at index ("
+                     << i << ", " << j << ")";
+                return;
+            }
+        }
+    }
+    cout << "Element not found";
+}
+
+int maxRowSum(int mat[][3], int rows, int colms) {
+    int maxSum = INT16_MIN;
+
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < colms; j++) {
+            rowSum += mat[i][j];
+        }
+        maxSum = max(maxSum, rowSum);
+    }
+    return maxSum;
+}
+
+int main() {
+    int mat[4][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {10,11,12}
+    };
+
+    int rows = 4;
+    int colms = 3;
+    int key = 8;
+    cout << maxRowSum(mat, rows, colms);
+
+    return 0;
+}
