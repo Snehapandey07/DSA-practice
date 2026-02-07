660671cb062824b84527f3b2b619672656b36ba3
Snehapandey07 <snehapandey.ss99@gmail.com>
2026-02-07 23:39:56 +0530

    lil update

--- Arrays/Test.cpp
+++ Arrays/Test.cpp
@@ -1,9 +1,19 @@
-/* Date: 28.01.26*/
-/* practice pairSum question*/
-#include <iostream>
+/*7 feb 2026*/
+/*Kadane's algorithm : maximum usbarray */
+#include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {8,8,5,9};
-    int target = 16;
-    
+    int size = 6;
+    int arr[6] = {10,49,89,65,4,9};
+    int max_sum = INT16_MIN;
+    int currentSum = 0;
+    for (int i = 0; i< size; i++){
+        currentSum += arr[i];
+        max_sum = max(currentSum, max_sum);
+        if(currentSum < 0){
+            currentSum = 0;
+        }
+        cout<< " max sum of max subarray is : "<<max_sum<< endl;
+        return 0;
+    }
 }
--- Arrays/strings.cpp
+++ Arrays/strings.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -13,4 +14,44 @@ int main(){
     cout<<endl;
     int len = s.length();
     cout<<"length of eneterd string: "<<len;
+*/
+
+/* Date: 6.01.26 */
+/*Program to check if a string is pakindrome , the time complexity of below program is 0(n)*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isAlphaNumeic(char ch){
+if(ch>= '0' && ch<= '9'){
+    (tolower(ch) >= 'a' && tolower(ch) <= 'b');
+    return true;
+}
+return false;
+}
+bool isPalindrome(string s){
+    int start = 0 , end = s.length();
+    while (start < end){
+    if(isAlphaNumeic(!s[start])){
+    start++; continue; 
+    }
+    if(isAlphaNumeic(!s[end])){
+    end--; continue; 
+    }
+    if(tolower(s[start] != tolower(s[end]))){
+        return false;
+    }
+    }
+    
+    }
+
+int main(){
+  string s = "racecar";
+  if (isPalindrome(s)) {
+        cout << "The entered string IS a palindrome " << endl;
+    } else {
+        cout << "The entered string is NOT a palindrome " << endl;
+    }
+  
 }
