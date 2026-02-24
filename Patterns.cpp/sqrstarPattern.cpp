f03fbbf7293600c3a50f9ccb9fd70de8b8f3742f
Snehapandey07 <snehapandey.ss99@gmail.com>
2026-02-24 08:47:20 +0530

    update

--- Patterns.cpp/sqrstarPattern.cpp
+++ Patterns.cpp/sqrstarPattern.cpp
@@ -22,12 +22,42 @@ for(int i = 0; i<row; i++){
     cout<< endl;
 }
 }
+void AscdnumberPattern(int n){
+    for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= i; j++) cout << j;
+    cout << endl;
+}
+}
+void starPyramid(char s, int row, int coulumn, int n){
+    for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= n-i; j++) cout << " ";       
+    for(int k = 1; k <= 2*i-1; k++) cout << "*"; 
+    cout << endl;
+}
+}
 int main(){
+    int n = 5; 
 int s = '*';
 int row = 4;
 int column = 4;
-// sqrStarPattern(s, row, column);
-// ascndngStarPattern(s, row ,column);
+cout<<"Square star Pattern"<<endl;
+sqrStarPattern(s, row, column);
+cout<<endl;
+cout<<"---------------------------"<<endl;
+cout<<"Ascending triangle star Pattern"<<endl;
+ascndngStarPattern(s, row ,column);
+cout<<endl;
+cout<<"---------------------------"<<endl;
+cout<<"Descentng trianle star pattern"<<endl;
 dscndngStarPattern(s, row,column);
+cout<<endl;
+cout<<"---------------------------"<<endl;
+cout<<"Ascending Number left triandle pattern "<<endl;
+AscdnumberPattern(n);
+cout<<endl;
+cout<<"---------------------------"<<endl;
+cout<<"Star Pyramid pattern"<<endl;
+starPyramid( s, row, column, n);
+cout<<"---------------------------"<<endl;
 return 0;
 }
