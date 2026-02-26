e9ad6c7e5b45c9c2574ee0ed2ba8bef2e71abf7a
Snehapandey07 <snehapandey.ss99@gmail.com>
2026-02-26 08:44:41 +0530

    update

--- revision/quicksort.cpp
+++ revision/quicksort.cpp
@@ -1,33 +1,35 @@
-/* Date : 26.02.26*/
-
 #include <iostream>
 using namespace std;
 
-int partition (int arr[], int start, int end ){
-int i = start -1 ; 
-int pivot = arr[end ];
-for ( j < start; j < end ; j++){
-    if (arr[j]<= pivot)
-}
+int partition(int arr[], int start, int end) {
+    int pivot = arr[end];
+    int i = start - 1;
+
+    for (int j = start; j < end; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[end]);
+    return i + 1;
 }
 
-void quicksort(int arr[], int start, int end, int pividx) {
-    if (start < end){
-pividx = partition(arr, start, end);
-quicksort(arr, start ,pividx-1);
-quicksort(arr, end ,pividx+1);
+void quicksort(int arr[], int start, int end) {
+    if (start < end) {
+        int pividx = partition(arr, start, end);
+
+        quicksort(arr, start, pividx - 1);
+        quicksort(arr, pividx + 1, end);
     }
-    
 }
 
 int main() {
-    int size = 7;
-    int arr[size] = {38, 27, 43, 3, 9, 82, 10};
-    int start = 
-    int end = start -1;
-    int pividx = ;
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = 7;
 
-    mergeSort(arr, 0, n - 1);
+    quicksort(arr, 0, n - 1);
 
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++)
