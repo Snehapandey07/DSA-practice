fcbbbd260f49285d3a5b03c6525735273edc675f
Sneha <snehapandey.ss99@gmail.com>
2026-03-26 22:32:11 +0530

    1 monotonic stack problem

---
+++ Stack n Queue/Monotonic Problems/nextGreaterEle.cpp
@@ -0,0 +1,40 @@
+/* Date : 26.03.26 */
+/* optimized approach */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findNGE(int arr[], int sz){
+    vector<int> nge(sz);
+    stack<int> st;
+
+    for(int i = sz - 1; i >= 0; i--){ //backward reversing {mistake : wrong operator} 
+        while(!st.empty() && st.top() <= arr[i]){
+            st.pop();// if stack is empty and there are smaller ele in it just pop it man they are not contributing towards the solution
+        }
+
+        if(st.empty()){
+            nge[i] = -1;
+        } else {
+            nge[i] = st.top();
+        }
+
+        st.push(arr[i]);
+    }
+
+    return nge;
+}
+
+int main(){
+    int sz = 12;
+    int arr[12] = {4,12,5,3,1,2,5,3,1,2,4,6};
+
+    vector<int> result = findNGE(arr, sz);
+
+    cout << "NGE's in the array:\n";
+    for(int x : result){
+        cout << x << " ";
+    }
+
+    return 0;
+}
