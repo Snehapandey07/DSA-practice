17f19acba9b6136c41a24eddb98c0a468c556e11
Sneha <snehapandey.ss99@gmail.com>
2026-03-27 08:20:15 +0530

    1 question solved!

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << __cplusplus;
+vector <int> findNGE(int ans[], int sz){
+    vector<int> nge(sz);
+    stack<int>st;
+    for(int i = sz-1; i>=0; i--){
+        while(!st.empty() && st.top()<=ans[i]){
+            st.pop();
+        }
+        if(st.empty()){
+        nge[i] = -1;
+        }else{
+            nge[i] = st.top();
+        }st.push(ans[i]); 
+    }
+    return nge;
+} 
+int main(){
+    int sz = 12;
+    int arr[12] = {4,12,5,3,1,2,5,3,1,2,4,6};
+
+    vector<int> result = findNGE(arr, sz);
+
+    cout << "NGE's in the array:\n";
+    for(int x : result){
+        cout << x << " ";
+    }
     return 0;
 }
