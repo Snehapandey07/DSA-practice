4afaec507be3a35b0f5dc10a31620c657055714d
Sneha <snehapandey.ss99@gmail.com>
2026-04-03 00:29:45 +0530

    updsate

--- Stack n Queue/Monotonic Problems/sumOfSubarrayRanges.cpp
+++ Stack n Queue/Monotonic Problems/sumOfSubarrayRanges.cpp
@@ -1,16 +1,46 @@
 /* Date: 2.04.26 */
-/* Sum of aubarray ranges Problem  */
+/* Sum of aubarray ranges Problem : Optimixed SOlution  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class solution{
-    public:
-    void func (int arr[] , int sz){
-
+class Solution {
+public:
+    int maxSumSubarr(int arr[], int n) {
+        int maxSum = INT_MIN;
+        for (int start = 0; start < n; start++) {
+            int current_sum = 0;
+            for (int end = start; end < n; end++) {
+                current_sum += arr[end];
+                maxSum = max(current_sum, maxSum);
+            }
+        }
+        return maxSum;
     }
-}
 
-int main(){
-    
+    int minSumSubarr(int arr[], int n) {
+        int minSum = INT_MAX;
+        for (int start = 0; start < n; start++) {
+            int current_sum = 0;
+            for (int end = start; end < n; end++) {
+                current_sum += arr[end];
+                minSum = min(current_sum, minSum);
+            }
+        }
+        return minSum;
+    }
+
+    int rangeSum(int arr[], int n) {
+        return maxSumSubarr(arr, n) - minSumSubarr(arr, n);
+    }
+};
+
+int main() {
+    int n = 4;
+    int arr[n] = {1, 4, 3, 2};
+
+    Solution obj;
+    cout << obj.rangeSum(arr, n);
+
+    return 0;
 }
