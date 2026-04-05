b11c4a1050e93e6e5c2ceba1c68427719bc90f0c
Sneha <snehapandey.ss99@gmail.com>
2026-04-05 19:28:46 +0530

    update

---
+++ Linked List/implement.cpp
@@ -0,0 +1,17 @@
+/* Date : 05.04.26 */
+/* Implementation  of Linked list : using dynamic memoer pointer */
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* y = new Node;
+    y->data = 2;
+    y->next = nullptr;
+
+    cout << y->data;
+}
