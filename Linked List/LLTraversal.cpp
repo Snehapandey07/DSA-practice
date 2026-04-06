05d6ac353cd666e2dde5702ae74ff44960497618
Sneha <snehapandey.ss99@gmail.com>
2026-04-06 23:25:45 +0530

    update

---
+++ Linked List/LLTraversal.cpp
@@ -0,0 +1,10 @@
+/*  Traversal logic 
+    start = head address
+    store start value in ptr
+    while loop to iterate over each node of linked list till it reaches the NULL
+    ptr = nextptr++;
+    end;
+    return ptr
+    
+    //by this logic we can traverse the linked list
+*/
