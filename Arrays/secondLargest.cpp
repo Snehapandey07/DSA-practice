430708b70c4b64a3f1c553944d906982917164b1
Sneha <snehapandey.ss99@gmail.com>
2026-05-19 11:36:01 +0530

    update

---
+++ Arrays/secondLargest.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+
+        int l = INT_MIN; // both starting from -infinity
+        int sl = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+          if (nums[i] > l){
+            mx = nums[i];
+          }
+            else if (nums[i] > sl && nums[i] != l) {
+                sl = nums[i];
+            }
+        }
+
+        if (sl == INT_MIN)
+            return -1;
+
+        return sl;
+    }
+};
+
+int main(){
+    vector<int> nums  = {4,7,2,72,89};
+    Solution s;
+    int ans = s.secondLargestElement(nums);
+    cout << "Second largest element "<<ans <<endl;
+    return 0 ;
+}
