db2137aca2ad7ce0a7f26053af6f046c14aefb49
Sneha <snehapandey.ss99@gmail.com>
2026-05-21 16:28:19 +0530

    update

---
+++ .vscode/SlidingWindow/SubstrwConcatOfallwrds.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+        vector<int> ans;
+
+        if (s.empty() || words.empty()) {
+            return ans;
+        }
+
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+
+        unordered_map<string, int> required;
+
+        // Store required frequencies
+        for (string word : words) {
+            required[word]++;
+        }
+
+        // Try every possible offset
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int count = 0;
+
+            unordered_map<string, int> window;
+
+            // Move right pointer word by word
+            for (int right = offset;
+                 right + wordLen <= s.size();
+                 right += wordLen) {
+
+                string word = s.substr(right, wordLen);
+
+                // Valid word
+                if (required.find(word) != required.end()) {
+
+                    window[word]++;
+                    count++;
+
+                    while (window[word] > required[word]) {
+
+                        string leftWord =
+                            s.substr(left, wordLen);
+
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+
+                        ans.push_back(left);
+
+                        string leftWord =
+                            s.substr(left, wordLen);
+
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+                }
+
+                else {
+
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
