880a33848219f1ae62c57d3a6f4f85777b76679d
Sneha <snehapandey.ss99@gmail.com>
2026-07-20 19:01:51 +0530

    update

---
+++ Trees/LCA.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode {
+    public : 
+    int val;
+    TreeNode*left;
+    TreeNode*right;
+
+    TreeNode (int x){
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+class Solution {
+  TreeNode*LCA(TreeNode *p, TreeNode *q, TreeNode *q){
+    if (root == nullptr || root == q || root = p){
+        return root;
+    }
+    TreeNode* LCA = LCA()
+  }
+};
