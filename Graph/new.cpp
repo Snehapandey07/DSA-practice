e37531ab2cff91bb464b31f13c46f766aa6187d8
Sneha <snehapandey.ss99@gmail.com>
2026-07-21 18:54:48 +0530

    update

--- Graph/new.cpp
+++ Graph/new.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n,m;
-    cin>>n>>m;
-    int adj [n+1][m+1];
-    for (int i = 0; i<m ; i++){
-        int u ,v;
-        cin>>u>>v;
-        adj[u][v] = 1;
-        adj[v][u] = 1;
+
+int main() {
+
+    int V = 5;      // Number of vertices
+
+    vector<vector<int>> adj(V);
+
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+
+    adj[0].push_back(2);
+    adj[2].push_back(0);
+
+    adj[0].push_back(3);
+    adj[3].push_back(0);
+
+    adj[2].push_back(4);
+    adj[4].push_back(2);
+
+    for(int i=0;i<V;i++){
+
+        cout<<i<<" -> ";
+
+        for(int neighbour : adj[i]){
+
+            cout<<neighbour<<" ";
+
+        }
+
+        cout<<endl;
     }
-    return 0;
+
 }
+// for directed graph simply remove the reverse edges
