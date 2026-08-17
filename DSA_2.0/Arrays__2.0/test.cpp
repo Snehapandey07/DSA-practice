0d3e6b585708ccb1511fb16f9a3aa38c05cdc110
Sneha <snehapandey.ss99@gmail.com>
2026-08-17 23:31:49 +0530

    update

--- DSA_2.0/Arrays__2.0/test.cpp
+++ DSA_2.0/Arrays__2.0/test.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    vector <int> arr = {9, 2, 15, 6, 8, 11, 4,12,14,24,56,98};
+
+int main() {
+    vector<int> arr = {9, 2, 15, 6, 8, 11, 2, 2, 4, 12, 14, 24, 56, 98};
+
     int n = arr.size();
+    int target = 2;
     int count = 0;
-    for(int i = 0; i<n; i++){
-            if (arr[i] % 2 == 0){
-         count ++;
-            }
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            count++;
         }
-         cout<<"count of even number in an array :"<< count ;
-    return 0 ;
+    }
+
+    cout << "Count of occurrences: " << count;
+
+    return 0;
 }
