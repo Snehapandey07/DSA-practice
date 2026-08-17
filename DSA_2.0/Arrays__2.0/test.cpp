9581cca80648d4a13e153e177b5079d1581a4670
Sneha <snehapandey.ss99@gmail.com>
2026-08-17 22:58:26 +0530

    update

--- DSA_2.0/Arrays__2.0/test.cpp
+++ DSA_2.0/Arrays__2.0/test.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main(){
     int sum = 0;
-    int largest = 0;
-    vector <int> arr = {9, 2, 15, 1, 7,18};
+    vector <int> arr = {9, 2, 15,5,-1,-2,1, 7,18};
+      int smallest = arr[0] ;
     int n = arr.size();
     for(int i = 0; i<n; i++){
-            if (arr[i] > largest){
-                largest = arr[i];
+            if (arr[i] < smallest){
+                smallest = arr[i];
             }
         }
-         cout<<"largest element in the array is :"<< largest;
+         cout<<"largest element in the array is :"<< smallest;
     return 0 ;
 }
