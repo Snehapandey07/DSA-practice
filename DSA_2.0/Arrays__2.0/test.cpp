6c403d2ef74ecce8aac4741f5a8433abaa447f64
Sneha <snehapandey.ss99@gmail.com>
2026-08-17 22:50:48 +0530

    update

--- DSA_2.0/Arrays__2.0/test.cpp
+++ DSA_2.0/Arrays__2.0/test.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main(){
     int sum = 0;
-    vector <int> arr = {9,2,5,1,3};
+    int largest = 0;
+    vector <int> arr = {9, 2, 15, 1, 7,18};
     int n = arr.size();
     for(int i = 0; i<n; i++){
-            sum += arr[i];
+            if (arr[i] > largest){
+                largest = arr[i];
+            }
         }
-    cout<<"sum of all elemnts of an array: "<<sum;
+         cout<<"largest element in the array is :"<< largest;
     return 0 ;
 }
