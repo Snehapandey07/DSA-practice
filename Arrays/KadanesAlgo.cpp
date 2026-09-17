556fdff240ec0019865ef842d206f66e8b28cea3
Sneha <snehapandey.ss99@gmail.com>
2026-09-17 19:11:03 +0530

    update

--- Arrays/KadanesAlgo.cpp
+++ Arrays/KadanesAlgo.cpp
@@ -4,24 +4,27 @@
 #include <iostream>
 #include <climits>
 #include <algorithm>
-
 using namespace std;
 
-int main(){
+int main() {
+
     int n = 6;
-    int arr [6] = {1,3,6,8,4,2};
+    int arr[6] = {-5, 2, 3, -10, 4, 5};
 
-    int maxSum = INT32_MIN;
-     int currentSum = 0;
+    int maxSum = INT_MIN;
+    int currentSum = 0;
 
-    for (int i =0; i < n ; i++){
-       currentSum += arr[i];
-       maxSum = max(currentSum, maxSum);
-       if (currentSum < 0){
-        currentSum = 0;
-       }
+    for (int i = 0; i < n; i++) {
 
+        currentSum += arr[i];
+        maxSum = max(currentSum, maxSum);
+
+        if (currentSum < 0) {
+            currentSum = 0;
+        }
     }
-    cout<< "Max sum of  Subarray is : "<< maxSum << endl; 
-return 0;
+
+    cout << "Max sum of Subarray is : " << maxSum << endl;
+
+    return 0;
 }
