65f42042821fc86f749693c4d872a1e9ab9c0048
Sneha <snehapandey.ss99@gmail.com>
2026-09-17 07:32:41 +0530

    changes

--- Arrays/ComputeXpowN.cpp
+++ Arrays/ComputeXpowN.cpp
@@ -1,6 +1,6 @@
 /* Date : 31.01.26 */
 /* To compute x^n using Binary Exponentiation */
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -37,5 +37,49 @@ int main() {
     double result = findPow(x, n);
     cout << x << " to the power " << n << " is " << result;
 
+    return 0;
+}  
+
+*/
+
+/* Different approach : 17.09.26 */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findpow(double x, int n) {
+
+        double ans = 1.0;
+        long long nn = n;
+
+        if (nn < 0) {
+            nn = -nn;
+        }
+
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            }
+            else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        if (n < 0) {
+            ans = 1.0 / ans;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    double x = 2;
+    int n = 10;
+    double res = s.findpow(x, n);
+    cout << "Answer : " << res;
     return 0;
 }
