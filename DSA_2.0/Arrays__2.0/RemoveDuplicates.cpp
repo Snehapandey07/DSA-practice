3e5230c30333db122b3646c6f94036197eb66ceb
Sneha <snehapandey.ss99@gmail.com>
2026-09-18 09:51:46 +0530

    update

--- DSA_2.0/Arrays__2.0/RemoveDuplicates.cpp
+++ DSA_2.0/Arrays__2.0/RemoveDuplicates.cpp
@@ -1,12 +1,41 @@
-/* Remove duplicates from sorted array */
+// /* Remove duplicates from sorted array */
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     vector<int> arr = {1, 1, 2, 2, 3, 3, 4};
+
+//     int n = arr.size();
+
+//     int j = 0;
+
+//     for (int i = 1; i < n; i++) {
+
+//         if (arr[i] != arr[j]) {
+//             j++;
+//             arr[j] = arr[i];
+//         }
+//     }
+
+//     cout << "Array after removing duplicates: ";
+
+//     for (int i = 0; i <= j; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    vector<int> arr = {1, 1, 2, 2, 3, 3, 4};
+
+    vector<int> arr = {8, 8, 2, 1, 2, 2};
+
+    sort(arr.begin(), arr.end());
 
     int n = arr.size();
-
     int j = 0;
 
     for (int i = 1; i < n; i++) {
