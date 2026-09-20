772f02f65e3c2a837f7dc5925ebe002a29958901
Sneha <snehapandey.ss99@gmail.com>
2026-09-20 09:33:06 +0530

    update

--- MySheet/BasicExampleLL.cpp
+++ MySheet/BasicExampleLL.cpp
@@ -23,13 +23,53 @@ int main() {
     third->next = nullptr;
 
     Node* head = first;
+    Node* head = nullptr;
+    Node* tail = nullptr;
 
+    /*traverse*/
     Node* temp = head;
-
     while (temp != nullptr) {
     cout << temp->data << " ";
     temp = temp->next;
     }
+
+    /*Insert at beginning*/
+    Node* newNode = new Node(5);
+    newNode->next = head;
+    head = newNode;
     
+    /*insert at end*/
+    if (head == nullptr) {
+    head = tail = newNode;
+    } else {
+    tail->next = newNode;
+    tail = newNode;
+    }
+
+    /*Search*/
+    int key = 20;
+    while (temp != nullptr) {
+    if (temp->data == key)
+        return true;
+    temp = temp->next;
+    }
+
+    /*delete first node*/
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    
+    /*Reverse ll*/
+    Node* prev = nullptr;
+    Node* curr = head;
+    while (curr != nullptr) {
+    Node* next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+    }
+    head = prev;
+    return false;
+
     return 0;
 }
