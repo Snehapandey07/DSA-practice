d49ce596841bf21dcc633531f0fda7829cb1fa31
Sneha <snehapandey.ss99@gmail.com>
2026-09-21 08:26:00 +0530

    update

--- MySheet/MajorityEle.cpp
+++ MySheet/MajorityEle.cpp
@@ -4,30 +4,59 @@
    count = its current votes */
 /* Why it works:- Different elements cancel each other out.*/
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//     int candidate = 0;
+//     int count = 0;
+
+//     for (int x : nums) {
+//         if (count == 0)
+//             candidate = x;
+
+//         if (x == candidate)
+//             count++;
+//         else
+//             count--;
+//     }
+//     return candidate;
+// }
+// };
+// int main(){
+// vector<int>nums = {8,546,894,879,77,7,7,7,7,1,7,8,2,5,5,2,2,2,0,8,8,8,7,7,7,7};
+// vector<int>no = {1,2,3,4,5,6};
+// Solution s ;
+// cout << "Majority Element: "<<s.majorityElement(nums);
+// return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-    int candidate = 0;
-    int count = 0;
-
-    for (int x : nums) {
-        if (count == 0)
-            candidate = x;
-
-        if (x == candidate)
-            count++;
-        else
-            count--;
+    public:
+    int majorityEle(vector<int>&nums){
+        int candidate = 0;
+        int count = 0;
+        for (int x : nums){
+            if (count == 0){
+                candidate = x;
+            }
+            if (x == candidate){
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate; 
     }
-    return candidate;
-}
 };
-int main(){
-vector<int>nums = {8,546,894,879,77,7,7,7,7,1,7,8,2,5,5,2,2,2,0,8,8,8,7,7,7,7};
+int main (){
+     vector<int>nums = {8,546,894,879,77,7,7,7,7,1,7,8,2,5,5,2,2,2,0,8,8,8,7,7,7,7};
 vector<int>no = {1,2,3,4,5,6};
 Solution s ;
-cout << "Majority Element: "<<s.majorityElement(nums);
-return 0;
+ cout << "Majority Element: "<<s.majorityEle(nums);
+ return 0;
 }
