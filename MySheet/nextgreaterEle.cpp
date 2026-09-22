5b08fabd52680903d2327039cd863989ad74542d
Sneha <snehapandey.ss99@gmail.com>
2026-09-22 10:06:16 +0530

    update

--- MySheet/nextgreaterEle.cpp
+++ MySheet/nextgreaterEle.cpp
@@ -29,3 +29,27 @@ int main() {
 
     return 0;
 }
+
+/*next greate ele 1 where it contains two vectors*/
+/*class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mp;
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+            if (st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+        return ans;
+    }
+};*/
