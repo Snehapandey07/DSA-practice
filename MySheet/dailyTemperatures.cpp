cc9809d5329fdfb2d8ef4ca139514bc4dbbb0031
Sneha <snehapandey.ss99@gmail.com>
2026-09-22 10:48:32 +0530

    update

---
+++ MySheet/dailyTemperatures.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> dailytemps(vector<int> temps) {
+        int n = temps.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && temps[st.top()] <= temps[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                ans[i] = st.top() - i;  //don't forget this
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> temps = {30, 60, 90};
+    Solution s;
+    vector<int> ans = s.dailytemps(temps);
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    return 0;
+}
