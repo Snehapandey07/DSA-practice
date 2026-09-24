dced8588d40ad62dc077c12eccefbcd98d169bee
Sneha <snehapandey.ss99@gmail.com>
2026-09-24 10:53:58 +0530

    update

--- Trees/validateBST.cpp
+++ Trees/validateBST.cpp
@@ -1,32 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node{
+struct node {
     int val;
-    node*left;
-    node*right;
-    node(int x){
-        data = x;
+    node* left;
+    node* right;
+
+    node(int x) {
+        val = x;
         left = nullptr;
         right = nullptr;
     }
 };
+
 class Solution {
 public:
     bool check(node* root, long long min, long long max) {
         if (root == nullptr)
             return true;
+
         if (root->val <= min || root->val >= max)
             return false;
+
         return check(root->left, min, root->val) &&
                check(root->right, root->val, max);
     }
+
     bool isValidBST(node* root) {
         return check(root, LLONG_MIN, LLONG_MAX);
     }
 };
-int main(){
-    
+int main() {
+    node* root = new node(2);
+    root->left = new node(1);
+    root->right = new node(3);
     Solution s;
+    if (s.isValidBST(root))
+        cout << "Valid BST";
+    else
+        cout << "Not a Valid BST";
     return 0;
-
 }
