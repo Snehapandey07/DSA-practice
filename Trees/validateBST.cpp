8b398591b1d9ec1b41a99757db6555fcc056bdca
Sneha <snehapandey.ss99@gmail.com>
2026-09-24 10:55:02 +0530

    update

--- Trees/validateBST.cpp
+++ Trees/validateBST.cpp
@@ -4,27 +4,22 @@ struct node {
     int val;
     node* left;
     node* right;
-
     node(int x) {
         val = x;
         left = nullptr;
         right = nullptr;
     }
 };
-
 class Solution {
 public:
     bool check(node* root, long long min, long long max) {
         if (root == nullptr)
             return true;
-
         if (root->val <= min || root->val >= max)
             return false;
-
         return check(root->left, min, root->val) &&
                check(root->right, root->val, max);
     }
-
     bool isValidBST(node* root) {
         return check(root, LLONG_MIN, LLONG_MAX);
     }
