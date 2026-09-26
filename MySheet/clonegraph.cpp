e439fb408b15a8605a45c7217764dc663f44c302
Sneha <snehapandey.ss99@gmail.com>
2026-09-26 11:36:20 +0530

    update

--- MySheet/clonegraph.cpp
+++ MySheet/clonegraph.cpp
@@ -93,14 +93,7 @@ public:
 };
 
 int main() {
-
-    /*
-          1 ---- 2
-          |      |
-          |      |
-          4 ---- 3
-    */
-    Node* n1 = new Node(1);
+  Node* n1 = new Node(1);
     Node* n2 = new Node(2);
     Node* n3 = new Node(3);
     Node* n4 = new Node(4);
